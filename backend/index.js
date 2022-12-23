cf1078234222b1eb24d303868b815288e0b99c61
vepada-sateesh <sateeshvepadas@gmail.com>
2022-12-23 09:43:25 +0530

    added

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,12 @@
 const express = require("express");
+// const server = express();
+const app = require('express')();
 const cors = require("cors");
 require("dotenv").config();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+
+const PORT = process.env.socket ||3000;
 
 const { questionRouter } = require("./routes/question.route");
 const { solutionRouter } = require("./routes/solution.route");
@@ -8,35 +14,58 @@ const { connection } = require("./config/database");
 const {userRouter} = require("./routes/user.route")
 
 
-const server = express();
-server.use(
+
+app.use(
   cors({
     origin: "*",
   })
 );
-server.use(express.json());
+app.use(express.json());
 
-server.get("/", (req, res) => {
-  res.status(200).send({ message: "Welcome to QuadNinja Server" });
+app.get("/", (req, res) => {
+  res.status(200).send({ message: "Welcome to QuadNinja app" });
 });
 
-server.use(userRouter);
+app.get('/chat', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
 
-  server.use("/questions", questionRouter);
-  server.use("/solutions", solutionRouter);
+app.use(userRouter);
+
+  app.use("/questions", questionRouter);
+  app.use("/solutions", solutionRouter);
+
+  io.on('connection', (socket) => {
+    socket.on('chat message', msg => {
+      io.emit('chat message', msg);
+    });
+  });
+
+// app.listen(process.env.PORT, async () => {
+//   try {
+//     await connection;
+//     console.log("db is connected successfully");
+//   } catch (err) {
+//     console.log("db is not connected ");
+//     console.log("err",err)
+//   }
 
 
 
-server.listen(process.env.PORT, async () => {
+//   console.log(`Server listning on http://localhost:${process.env.PORT}`);
+// });
+// http.listen(3000, () => {
+//   console.log(`Socket.IO server running at http://localhost:${3000}/`);
+// });
+
+app.start = app.listen =async function () {
   try {
-    await connection;
-    console.log("db is connected successfully");
-  } catch (err) {
-    console.log("db is not connected ");
-    console.log("err",err)
-  }
-
-
-
-  console.log(`Server listning on http://localhost:${process.env.PORT}`);
-});
+        await connection;
+        console.log("db is connected successfully");
+      } catch (err) {
+        console.log("db is not connected ");
+        console.log("err",err)
+      }
+  return http.listen.apply(http,arguments)
+}
+app.start(process.env.PORT)
